d67c13bdcb3951d7eafa0727141607dedf203db1
seonghoon07 <chltjdgns1009@gmail.com>
2025-04-07 20:31:04 +0900

    feat(#1) :: 데이터베이스 연동 및 users,places 엔티티 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmModuleOptions } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
+import { PlaceModule } from './places/place.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmModuleOptions), UsersModule, PlaceModule],
   controllers: [AppController],
   providers: [AppService],
 })
