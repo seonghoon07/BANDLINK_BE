52091b5802230e0310987ef0e3a015b847731108
seonghoon07 <chltjdgns1009@gmail.com>
2025-04-08 16:53:47 +0900

    feat(#3) :: Google OAuth 토큰 발급 구현

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { GoogleStrategy } from '@/src/auth/strategies/google.strategy';
+
+@Module({
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, GoogleStrategy, JwtStrategy],
+})
+export class AuthModule {}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy as JwtStrategyBase } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+interface JwtPayload {
+  sub: string;
+  name: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(JwtStrategyBase) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET')!,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return {
+      userId: payload.sub,
+      name: payload.name,
+    };
+  }
+}
