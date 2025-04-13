2c2ddc8135c86b371db943ac10cd0dbc57bf40ad
seonghoon07 <chltjdgns1009@gmail.com>
2025-04-13 23:45:18 +0900

    feat(#9) :: update-user dto 설정

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, ValidateIf } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
+  nickname?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  roles?: ('FAN' | 'BAND' | 'PLACE_OWNER')[];
+
+  @ValidateIf((object: UpdateUserDto) => !!object.roles?.includes('BAND'))
+  @IsString({ message: '밴드 역할에는 bandname이 필수입니다.' })
+  bandname?: string;
+}
